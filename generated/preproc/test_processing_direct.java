587678a4f59a210bc63cc6b85b6e7cad49611619
dichenli <dichenli@sas.upenn.edu>
2014-10-25 20:45:48 -0400

    commit again after change username

--- generated/preproc/test_processing_direct.java
+++ generated/preproc/test_processing_direct.java
@@ -23,6 +23,7 @@ public void setup(){
  
 public void draw(){ 
 	ellipse(50,50,80,80); 
+	ellipse(100,100,80,80); 
 } 
     static public void main(String args[]) {
         PApplet.main(new String[] { "--bgcolor=#ECE9D8", "test_processing_direct" });
